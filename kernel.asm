ac65185ffb6f6fd7e084e73be07feb721bb94865
Paul Caron <paul.a.caron@gmail.com>
2024-08-12 19:47:22 -0500

    menu

--- kernel.asm
+++ kernel.asm
@@ -3,20 +3,34 @@ start: jmp main;
 
 kernel_version: db "KERNEL: ChillPillOS v0.0.0", 0x0A, 0x0D, 0x00
 canonical: db 0x00 ;
+clock_string: db "00:00:00", 0x00
+menu_str: db "Menu : ", 0x0A, 0x0D
+          db "   1 - PrintTime", 0x0A, 0x0D
+          db "   2 - Reboot", 0x0A, 0x0D
+          db "   3 - REPL", 0x0A, 0x0D, 0x00
 
 io:
   %include "io.asm"
+rtc:
+  %include "rtc.asm"
+sys:
+  %include "sys.asm"
+repl:
+  %include "repl.asm"
+menu:
+  %include "menu.asm"
 
 main:
-  cld ;
+  ; setup segments ;
+  mov ax, 0x50 ;
+  mov ds, ax ;
+
   ; set video mode
   mov ah, 0x00 ;
   mov al, 0x03 ; text color mode
   int 0x10 ;                     ;
 
-  mov ax, 0x50 ;
-  mov ds, ax ;
-
+  ; print version ;
   mov si, kernel_version; 
   call Print ;
 
