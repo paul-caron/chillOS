c76fbf3c405aa29d748034870b93b187c90eda36
Paul Caron <paul.a.caron@gmail.com>
2024-08-13 21:22:45 -0500

    disk reading error handling

--- bootloader.asm
+++ bootloader.asm
@@ -2,6 +2,11 @@ org 0x7c00
 bits 16
 start: jmp main
 
+io:
+  %include "io.asm"
+
+disk_error_string: db "BOOTLOADER: reading from disk error.", 0x0A, 0x0D, 0x00
+canonical: db 0x00 ;
 ;; main
 main:
   cli ;
@@ -18,9 +23,17 @@ main:
   mov dl, 0x00 ;drive number
   mov ah, 0x02 ;read sectors code
   int 0x13     ;bios interrupt
+  ;disk reading error handling
+  jc disk_error ;
   ;execute kernel on sector 2
   jmp 0x50:0x00 ;
   hlt ; halt the system
+
+disk_error:
+  mov si, disk_error_string;
+  call Print ;
+  hlt ;
+
 ; Clear rest of bytes until last 2 bytes of 512 sector
 ; last two byte be the boot signature
 signature:
